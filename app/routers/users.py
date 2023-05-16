24861037f5e14075b5828bfe1f91e97f5288d8f2
Elodie62 <elodie602@free.fr>
2023-05-16 23:37:22 +0200

    user with check status

--- app/routers/users.py
+++ app/routers/users.py
@@ -1,11 +1,12 @@
 #system import (import de base de python)
-from typing import List
+from typing import List, Annotated
 #lib import
-from fastapi import APIRouter, status, Response, HTTPException
+from fastapi import APIRouter, Depends, status, Response, HTTPException
 from passlib.context import CryptContext
 #local import 
-from models.users import User
+from models.users import User,UserLogin
 import internals.database
+from internals.auth import decode_token
 
 
 router=APIRouter()
@@ -14,7 +15,7 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 async def getAllUserInDB() -> List[User]:
     database = internals.database.connect()
     request= database.cursor( dictionary=True )    
-    request.execute("SELECT * FROM users")
+    request.execute("SELECT * FROM users INNER JOIN company ON users.idCompany=company.id INNER JOIN status ON users.idStatus= status.id")
     users = request.fetchall()
     request.close()
     database.close()
@@ -24,8 +25,15 @@ def get_password_hash(password):
     return pwd_context.hash(password)
 
 @router.get("/users", responses={status.HTTP_204_NO_CONTENT:{}})
-async def getAllUser()-> List[User]:
+async def getAllUser(currentUser: Annotated[User, Depends(decode_token)])-> List[UserLogin]:
+    print(currentUser)
+    if (currentUser.nameStatus != "admin" and currentUser.nameStatus != "maintener"):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail="Unauthorized")
     users= await getAllUserInDB()
+    if (currentUser.nameStatus == "admin"):
+        return list(
+            filter(lambda user: user["nameCompany"] == currentUser.nameCompany,  users))
 
     if users== None:
         return Response(status_code=status.HTTP_204_NO_CONTENT)
@@ -33,7 +41,7 @@ async def getAllUser()-> List[User]:
 
 
 @router.post("/users", status_code=status.HTTP_201_CREATED)
-async def createUser(userToCreate:User):
+async def createUser(currentUser: Annotated[User, Depends(decode_token)],userToCreate:User):
     database = internals.database.connect()
     request= database.cursor( dictionary=True )    
     users= await getAllUserInDB()
@@ -52,22 +60,29 @@ async def createUser(userToCreate:User):
     
     
 @router.put("/users/{email}", status_code=status.HTTP_201_CREATED)
-async def updateUser(email: str, userToUpdate: User):
-   users= await getAllUserInDB()
-   database = internals.database.connect()
-   request= database.cursor( dictionary=True )   
-   for user in users:
-       if user['email']==email:
-           sql_query = "UPDATE users SET firstname=%s, lastname=%s, email=%s, phone=%s, password=%s, idStatus=%s,idCompany=%s WHERE email=%s"
-           user_data = (userToUpdate.firstname,userToUpdate.lastname, userToUpdate.email, userToUpdate.phone, userToUpdate.password, userToUpdate.idStatus, userToUpdate.idCompany, email)
-           request.execute(sql_query, user_data)
-           database.commit()
-           request.close()
-           database.close()
+async def updateUser(currentUser: Annotated[User, Depends(decode_token)], email: str, userToUpdate: User):
+    if (currentUser.nameStatus != "admin" and currentUser.nameStatus != "maintener"):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail="Unauthorized")
+    
+    users= await getAllUserInDB()
+    database = internals.database.connect()
+    request= database.cursor( dictionary=True )   
+    for user in users:
+        if user['email']==email:
+            sql_query = "UPDATE users SET firstname=%s, lastname=%s, email=%s, phone=%s, password=%s, idStatus=%s,idCompany=%s WHERE email=%s"
+            user_data = (userToUpdate.firstname,userToUpdate.lastname, userToUpdate.email, userToUpdate.phone, userToUpdate.password, userToUpdate.idStatus, userToUpdate.idCompany, email)
+            request.execute(sql_query, user_data)
+            database.commit()
+            request.close()
+            database.close()
            
     
 @router.delete("/users/{email}", responses={status.HTTP_204_NO_CONTENT:{}})
-async def delete_user(userEmail: str) :
+async def delete_user(currentUser: Annotated[User, Depends(decode_token)],userEmail: str) :
+    if (currentUser.nameStatus != "admin" and currentUser.nameStatus != "maintener"):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail="Unauthorized")
     users= await getAllUserInDB()
     database = internals.database.connect()
     request= database.cursor( dictionary=True )   
