e7376f467a0e8adcd28f1e7f2fcce453cf21f3bc
Elodie62 <elodie602@free.fr>
2023-05-17 12:09:23 +0200

    add companies and planning in main

--- app/main.py
+++ app/main.py
@@ -6,9 +6,13 @@ from fastapi import FastAPI, status
 from internals import auth
 from routers import users
 from routers import activities
+from routers import companies
+from routers import planning
 app = FastAPI()
 
 
 app.include_router(users.router, tags=["users"])
 app.include_router(auth.router, tags=["auth"])
 app.include_router(activities.router, tags=["activities"])
+app.include_router(planning.router, tags=["planning"])
+app.include_router(companies.router, tags=["companies"])
