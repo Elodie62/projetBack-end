0eae3491192a480947635665df24f0eddbeaccdd
Elodie62 <elodie602@free.fr>
2023-05-17 10:57:11 +0200

    add activities in main

--- app/main.py
+++ app/main.py
@@ -1,13 +1,14 @@
 #system import 
 #lib import
+
 from fastapi import FastAPI, status
 #local import
 from internals import auth
 from routers import users
+from routers import activities
 app = FastAPI()
 
 
 app.include_router(users.router, tags=["users"])
-
-
 app.include_router(auth.router, tags=["auth"])
+app.include_router(activities.router, tags=["activities"])
